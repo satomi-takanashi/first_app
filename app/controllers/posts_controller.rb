94aa16a2e54dfd094494d0f530803c55ebdda1dd
satomi-takanashi <74594934+satomi-takanashi@users.noreply.github.com>
2020-11-17 19:19:43 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController 
+  def index 
+    @posts = Post.all
+  end
+
+  def create
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
